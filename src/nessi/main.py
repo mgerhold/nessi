893514f9e330ae74ee8cf8899f78210a684d486c
Michael Gerhold <michael.gerhold@web.de>
2026-01-06 14:17:20 +0100

    feat: allow running multiple examples programs in main

--- src/nessi/main.py
+++ src/nessi/main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from typing import Final
+from typing import NamedTuple
+from typing import final
 
 from nassi_shneiderman_generator.latex import render_latex_to_pdf
 
@@ -13,35 +14,48 @@ from nessi.statements import While
 from nessi.value import Value
 
 
-def main() -> None:
-    program: Final = Program(
-        [
-            Input("n", int),
-            Input("binary", ArrayType(int, "n")),
-            Assign("decimal", 0),
-            Assign("power_of_2", 1),
-            Assign("i", Variable("n") - 1),
-            While(Variable("i") >= 0).Repeat(
-                Assign(
-                    "decimal",
-                    Variable("decimal") + Variable("binary")[Variable("i")] * Variable("power_of_2"),
+@final
+class Example(NamedTuple):
+    program: Program
+    input_values: dict[str, Value]
+
+
+EXAMPLES = [
+    Example(
+        program=Program(
+            [
+                Input("n", int),
+                Input("binary", ArrayType(int, "n")),
+                Assign("decimal", 0),
+                Assign("power_of_2", 1),
+                Assign("i", Variable("n") - 1),
+                While(Variable("i") >= 0).Repeat(
+                    Assign(
+                        "decimal",
+                        Variable("decimal") + Variable("binary")[Variable("i")] * Variable("power_of_2"),
+                    ),
+                    Assign("power_of_2", Variable("power_of_2") * 2),
+                    Assign("i", Variable("i") - 1),
                 ),
-                Assign("power_of_2", Variable("power_of_2") * 2),
-                Assign("i", Variable("i") - 1),
-            ),
-            Print("Die Dezimalzahl ist {decimal}."),
-        ]
+                Print("Die Dezimalzahl ist {decimal}."),
+            ]
+        ),
+        input_values={
+            "n": 8,
+            "binary": [0, 1, 0, 0, 1, 1, 0, 1],
+        },
     )
-    input_values: Final[dict[str, Value]] = {
-        "n": 8,
-        "binary": [0, 1, 0, 0, 1, 1, 0, 1],
-    }
-    output: Final = program.run(input_values, verbose=False)
-    diagram: Final = program.generate_diagram()
-    latex_code: Final = diagram.emit()
-    print(latex_code)
-    render_latex_to_pdf(latex_code, Path("test.pdf"))
-    print(f"Program output:\n'''\n{output}'''")
+]
+
+
+def main() -> None:
+    for i, example in enumerate(EXAMPLES):
+        output = example.program.run(example.input_values, verbose=False)
+        diagram = example.program.generate_diagram()
+        latex_code = diagram.emit()
+        print(latex_code)
+        render_latex_to_pdf(latex_code, Path(f"test{i}.pdf"))
+        print(f"Program output:\n'''\n{output}'''")
 
 
 if __name__ == "__main__":
